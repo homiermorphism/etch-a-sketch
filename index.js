4270a28b72b210a2c345bbbd57344e03a18bc0b5
Samantha <samanthahomier@yahoo.com>
2020-01-17 21:46:40 -0800

    added basic grayscale sketching; changed code for border and screen

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const gridContainer = document.querySelector('.grid-container');
+const options = document.getElementsByName('sketch-type');
+const cells = document.getElementsByClassName('grid-item');
+
 
 function makeGrid(n) {
   gridContainer.style.setProperty('--grid-amount', n);
   for (i=0; i < (n*n); i++) {
     const cell = document.createElement('div');
-    gridContainer.appendChild(cell).className = "grid-item";
+    gridContainer.appendChild(cell).className = 'grid-item';
+    cell.addEventListener('mouseover', (e) => {
+      e.currentTarget.style.backgroundColor = 'gray';
+    });
   }
 }
+
+
 
 makeGrid(16);
